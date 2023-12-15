b7d03a2aae5066b67297832972bd59d9a72417cf
Daniel Vall <DanielVall2001@gmail.com>
2023-12-16 01:41:08 +0600

    Catch up to day 15, finally

--- 15_Lens_library.py
+++ 15_Lens_library.py
@@ -1,5 +1,48 @@
 
 
-with open('Advent_of_code_2023/data/input_day_15.txt', 'r') as file:
+def hash_char(key: str) -> int:
 
-    print(file)
+    global boxes
+    global cach
+
+    if key in cach:
+        return cach[key]
+
+    res: int = 0
+
+    for char in key:
+        res += ord(char)
+        res *= 17
+        res %= 256
+
+    cach[key] = res
+
+    return res
+
+
+long_fucking_line = open(
+    'Advent_of_code_2023/data/input_day_15.txt', 'r'
+).read().strip().split(',')
+
+boxes: list[dict[str: int]] = [{} for _ in range(256)]
+cach: dict[str: int] = {}
+result: int = 0
+
+for lens in long_fucking_line:
+    if '-' in lens:
+        key = lens[:-1]
+        box = boxes[hash_char(key)]
+        if key in box:
+            box.pop(key)
+    else:
+        key = lens[:-2]
+        box = boxes[hash_char(key)]
+        box[key] = lens[-1]
+
+for number_box, box in enumerate(boxes):
+    order_lens = 1
+    for key, val in box.items():
+        result += (number_box+1) * order_lens * int(val)
+        order_lens += 1
+
+print(result)
